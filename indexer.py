173cd8b4f28fc6029c49cc138fcbb8652ef16d49
Zaman <7amaaan@gmail.com>
2019-08-14 11:05:10 +0430

    handle no content type

--- indexer.py
+++ indexer.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 
-import requests
+import json
 from urllib.parse import urlparse
+import requests
 from bs4 import BeautifulSoup
 from options import url, root
 
+def human_readable(size):
+    size = int(size)
+    for unit in ['', 'K', 'M', 'G', 'T']:
+        if size < 1024.0:
+            return "{:03.2f} {}B".format(size, unit)
+        size = size / 1024.0
 
 def get_links(url):
     page = requests.get(url)
@@ -35,15 +42,35 @@ def is_file(url):
         return False
     return True
 
-def get_files(url, textfile):
+def get_files(url, db):
     links = get_links(url)
     for link in links:
         full, cat = full_url_and_cat(url, link)
         if cat == 'subfolder':
-            get_files(full, textfile)
+            get_files(full, db)
         elif cat == 'other':
-            print(full)
-            textfile.write(full+'\n')
+            headers = requests.head(full).headers
+            if not headers.__contains__('Content-Type'):
+                headers['Content-Type'] = 'Null'
+            if not headers.__contains__('Content-Length'):
+                    headers['Content-Type'] = '0'
+            db.append(
+                {
+                    'url': full,
+                    'type': headers['Content-Type'],
+                    'size': headers['Content-Length'],
+                    'hsize': human_readable(headers['Content-Length'])
+                }
+            )
+        print('.', end='', flush=True)
+    print()
+    return db
 
+db = list()
+db = get_files(url, db)
+links = [item['url']+'\n' for item in db]
+print(human_readable(sum([int(item['size']) for item in db])))
+with open('db.json', 'w') as dbjson:
+    json.dump(db, dbjson, indent=2)
 with open('links.txt', 'w') as textfile:
-    get_files(url, textfile)
+    textfile.writelines(links)
